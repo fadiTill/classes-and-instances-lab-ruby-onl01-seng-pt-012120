c1b6ce6b017f48b787339ab89b759d90242649d8
fadimata tillman <fadi.tillman@gmail.com>
2020-02-03 19:59:14 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,27 @@
+ # class Dog
+ #  def fido
+ #   puts "one "
+ # end
+
+ class Dog
+ end
+
+  #  fido = Dog.new
+ # def fido
+ # fido = new.Dog
+ # end
+ # end # new instance of dog
+#  def snoopy
+#   puts "two"
+#  end
+#
+#  def lassie
+#   puts "tree"
+#  end
+# end
+
+
+ # name = Dog.new
+ # name.fido
+ # name.snoopy
+ # name.lassie
---
+++ lib/person.rb
@@ -0,0 +1,15 @@
+
+class Person
+  def adele_goldberg
+   puts "women"
+ end
+
+def alan_kay
+  puts "Men"
+  end
+end
+
+
+ name = Person.new
+ name.adele_goldberg
+ name.alan_kay

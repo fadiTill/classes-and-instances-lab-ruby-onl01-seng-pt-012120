42e23af3488cb74a41824f9b45e5f8c5667235f8
fadimata tillman <fadi.tillman@gmail.com>
2020-02-03 20:08:28 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,15 +1,6 @@
 
 class Person
-  def adele_goldberg
-   puts "women"
- end
-
-def alan_kay
-  puts "Men"
   end
-end
 
-
- name = Person.new
- name.adele_goldberg
- name.alan_kay
+adele_goldberg =    Person.new
+ alan_kay
